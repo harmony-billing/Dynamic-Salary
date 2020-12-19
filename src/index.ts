c4b8b70c5e8b810919e0d3fda9257807af9efde5
Kevin <xxkevtvxx@gmail.com>
2020-12-19 10:44:33 +0100

    fixed and extended functionality

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,19 @@ export class DynamicSalary {
         
     }
 
-    getTransactionDataFromAdresses = async (senderAdress: string, receiverAdress: string, from: Date, to: Date) => {
-        let output = await utils.getAdressType(senderAdress)
-        return output
+    getTransactionDataFromAdresses = async (senderAdress: string, receiverAdress: string, from: Date, to: Date) => {        
+        let senderAdressType = await utils.getAdressType(senderAdress)
+        let receiverAdressType = await utils.getAdressType(receiverAdress)
+        return  { 
+            "senderType": senderAdressType,
+            "receiverType": receiverAdressType
+        }
     }
 
 }
 
 let ds = new DynamicSalary()
 
-let s = ds.getTransactionDataFromAdresses("test", "test", new Date(), new Date())
-
-console.log(s)
-
-// I HATE ASYNC FUNCTION...............
+ds.getTransactionDataFromAdresses("0x4f6742badb049791cd9a37ea913f2bac38d01279", "0x4f6742badb049791cd9a37ea913f2bac38d01279", new Date(), new Date()).then((data) => {
+    console.log(data)
+})
