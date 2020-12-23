256ac0af1ff4db5c5b23c0bc95215c7d6951a115
Kevin <xxkevtvxx@gmail.com>
2020-12-23 19:36:01 +0100

    added sendOneToOne function to DynamicSalary class

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,19 @@ import { mainnet } from './configs/mainnet'
 import { testnet } from './configs/testnet'
 import * as Data from './utils'
 import { OperationCanceledException } from 'typescript'
+import { Wallet } from '@harmony-js/account'
+import { Messenger, HttpProvider } from '@harmony-js/network'
+import {
+    ChainID,
+    ChainType,
+    hexToNumber,
+    numberToHex,
+    hexToBN,
+    fromWei,
+    Units,
+    Unit,
+} from '@harmony-js/utils'
+import { ContractFactory } from '@harmony-js/contract'
 
 export enum TYPE {
     ETH = "Ethereum",
@@ -136,18 +149,66 @@ export class DynamicSalary {
             return data
 
         }
+
+        sendOneToOne = (
+            privateKey: string,
+            receiverAddress: string,
+            amount: number,
+        ) => {
+            let wallet = new Wallet(
+                new Messenger(
+                    new HttpProvider(this.configs.hmyConfig.nodeURL),
+                    ChainType.Harmony,
+                    this.configs.hmyConfig.testnet ? ChainID.HmyTestnet : ChainID.HmyMainnet
+                )
+            )
+            let factory = new ContractFactory(wallet)
+            let contractJson = require('@harmony-js/contract/Counter.json')
+            let contractAddress = "0xb0e18106520d05adA2C7fcB1a95f7db5e3f28345"
+            let contract = factory.createContract(contractJson.abi, contractAddress)
+        
+            contract.wallet.addByPrivateKey(privateKey)
+        
+            let gasP = { gasPrice: '0x3B9ACA00' }
+            let gas2 = { gasPrice: this.configs.hmyConfig.gasPrice, gasLimit: this.configs.hmyConfig.gasLimit }
+        
+            let bytecode = { data: contractJson.bytecode }
+        
+            contract.methods.contractConstructor(bytecode).estimateGas(gasP).then(gas => {
+                let gas3 = { ...gas2, gasLimit: hexToNumber(gas) }
+                contract.methods.contractConstructor(bytecode).send(
+                {
+                    gasPrice: gas3.gasPrice,
+                    value: amount,
+                    gasLimit: gas3.gasLimit,
+                    to: receiverAddress,
+                    shardID: 0,
+                    toShardID: 0,
+                }).then((res) => {
+                    console.log(res["transaction"])
+                })
+            })
+        
+        }
+
 }   
 
 let ds = new DynamicSalary(testnet)
 
-ds.getTransactionData(
+/*ds.getTransactionData(
     TYPE.ONE,
     "one1akph3q7a0vtfzad2lau0cfvsvkrnc5fzf487ly",
     TYPE.ETH,
     "0x430506383F1Ac31F5FdF5b49ADb77faC604657B2", 
 ).then((data) => {
     console.log(data)
-})
+})*/
+
+ds.sendOneToOne(
+    "0x936224fc6acd1d8e4dab100c054ed7305acf520207b2f0c15257d570d5fd56de",
+    "one1xlk8jnxw68nwksxtqt39t0ggghfnuex5pak7j4",
+    100000000000
+)
 
 
 
