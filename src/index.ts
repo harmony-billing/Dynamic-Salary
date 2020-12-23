a1031c7a154cb1b2757cff9d6dc29d8a61644e42
Kevin <xxkevtvxx@gmail.com>
2020-12-23 16:34:19 +0100

    added ONE to ONE and ETH to ETH data receiver

--- src/index.ts
+++ src/index.ts
@@ -28,34 +28,19 @@ export class DynamicSalary {
         senderAddress: string, 
         receiverType: TYPE,
         receiverAddress: string, 
-        from?: Date, 
-        to?: Date
         ) => {
-            let hmy = new Harmony(
-                this.configs.hmyConfig.nodeURL,
-                {
-                    chainType: ChainType.Harmony,
-                    chainId: this.configs.hmyConfig.testnet ? ChainID.HmyTestnet : ChainID.HmyMainnet
-                }
-            )
-
-            let eth = new Harmony(
-                this.configs.ethConfig.nodeURL,
-                {
-                    chainType: ChainType.Ethereum,
-                    chainId: this.configs.ethConfig.testnet ? ChainID.Kovan : ChainID.EthMainnet
-                }
-            )
             
-            let senderData = {}
+            let data = []
+
+            let senderData: any
 
             if (senderType == TYPE.ETH) {
-                Data.getEthData(this.configs, senderAddress).then((res) => {
-                    console.log(res)
+                senderData = await Data.getEthData(this.configs, senderAddress, 0).then((res) => {
+                    return res
                 })
             } else if (senderType == TYPE.ONE) {
-                Data.getHmyData(this.configs.hmyConfig.nodeURL, senderAddress).then((res) => {
-                    senderData = res
+                senderData = await Data.getHmyData(this.configs.hmyConfig.nodeURL, senderAddress).then((res) => {
+                    return res
                 })
             }
 
@@ -63,33 +48,123 @@ export class DynamicSalary {
                 throw OperationCanceledException
             }
 
-            let receiverData = {}
+            let receiverData: any
 
-            if (receiverType = TYPE.ETH) {
-                Data.getEthData(this.configs, senderAddress).then((res) => {
-                    console.log(res)
+            if (receiverType == TYPE.ETH) {
+                receiverData = await Data.getEthData(this.configs, receiverAddress, 0).then((res) => {
+                    return res
                 })
-            } else if (receiverType = TYPE.ONE) {
-                Data.getHmyData(this.configs.hmyConfig.nodeURL, receiverAddress).then((res) => {
-                    receiverData = res
+            } else if (receiverType == TYPE.ONE) {
+                receiverData = await Data.getHmyData(this.configs.hmyConfig.nodeURL, receiverAddress).then((res) => {
+                    return res
                 })
             }
 
             if (!receiverData) {
                 throw OperationCanceledException
             }
+
+            if (senderType == TYPE.ONE && receiverType == TYPE.ONE) {
+                let max = []
+                let min = []
+                let from = 'from'
+                let to = 'to'
+
+                if(senderData.length > receiverData.length) {
+                    max = senderData
+                    min = receiverData
+                } else {
+                    max = receiverData
+                    min = senderData
+                    from = 'to'
+                    to = 'from'
+                }
+                
+                let transactions = []
+
+                for (let i = 0; i < max.length; i++) {
+                    for (let j = 0; j < min.length; j++) {
+                        if (max[i][to] == min[j][to] && max[i][from] == min[j][from]) {
+                            transactions.push(max[i])
+                        }
+                    }
+                }
+
+                transactions = transactions.filter(function(elem, index, self) {
+                    return index === self.indexOf(elem);
+                })
+
+                data = transactions
+
+
+            } else if (senderType == TYPE.ETH && receiverType == TYPE.ETH) {
+
+                let max = []
+                let min = []
+                let from = 'from'
+                let to = 'to'
+                if(senderData.length > receiverData.length) {
+                    max = senderData
+                    min = receiverData
+                } else {
+                    max = receiverData
+                    min = senderData
+                    from = 'to'
+                    to = 'from'
+                }
+                
+                let transactions = []
+
+                for (let i = 0; i < max.length; i++) {
+                    for (let j = 0; j < min.length; j++) {
+                        if (max[i][to] == min[j][to] && max[i][from] == min[j][from]) {
+                            transactions.push(max[i])
+                        }
+                    }
+                }
+
+                transactions = transactions.filter(function(elem, index, self) {
+                    return index === self.indexOf(elem);
+                })
+
+                data = transactions
+
+            } else if (senderType == TYPE.ETH && receiverType == TYPE.ONE) {
+
+            } else if (senderType == TYPE.ONE && receiverType == TYPE.ETH) {
+
+            }
+
+            /*receiverData.forEach(element => {
+                console.log(element['to'])
+            });
+            let contractAddresses = [
+                this.configs.ethConfig.contracts.busd,
+                this.configs.ethConfig.contracts.busdManager,
+                this.configs.ethConfig.contracts.erc20Manager,
+                this.configs.ethConfig.contracts.link,
+                this.configs.ethConfig.contracts.linkManager
+            ]
+            //console.log(contractAddresses)
+            senderData.forEach(element => {
+                //console.log(element['contractAddress'])
+                if (contractAddresses.includes(element["from"])) {
+                    console.log(element)
+                }
+            });*/
+            
+            return data
+
         }
-    }   
+}   
 
 let ds = new DynamicSalary(testnet)
 
 ds.getTransactionData(
     TYPE.ETH,
     "0x4778D03bB3E169b920Cbf826F9A931A15574fE28", 
-    TYPE.ONE,
-    "one1xlk8jnxw68nwksxtqt39t0ggghfnuex5pak7j4", 
-    new Date(), 
-    new Date()
+    TYPE.ETH,
+    "0x89Cb9b988ECe933becbA1001aEd98BdAa660Ef29", 
 ).then((data) => {
     console.log(data)
 })
