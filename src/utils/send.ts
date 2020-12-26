1707ae6771c0a06aa620a4a37cc020974cda849d
Kevin <xxkevtvxx@gmail.com>
2020-12-26 09:05:35 +0100

    added ETH to ETH transaction option

--- src/utils/send.ts
+++ src/utils/send.ts
@@ -4,6 +4,10 @@ const {
   ChainType,
   Unit,
 } = require('@harmony-js/utils');
+const {
+    getAddressFromPrivateKey
+} = require('@harmony-js/crypto');
+const Web3 = require("web3")
 
 export async function sendOne(senderPK, receiverAddress, amount, config) {
     let hmy = new Harmony(
@@ -29,4 +33,26 @@ export async function sendOne(senderPK, receiverAddress, amount, config) {
     let txnHash = await hmy.blockchain.sendTransaction(signedTxn)
     return txnHash.result
 
+}
+
+export async function sendEth(senderPK, receiverAddress, amount, config) {
+    let web3 = new Web3(config.ethConfig.nodeURL)
+    let senderAddress = getAddressFromPrivateKey(senderPK)
+
+    let createTransaction = await web3.eth.accounts.signTransaction(
+        {
+            from: senderAddress,
+            to: receiverAddress,
+            value: web3.utils.toWei(String(amount), 'ether'),
+            gas: config.ethConfig.gasLimit,
+            gasPrice: config.ethConfig.gasPrice
+        },
+        senderPK
+    )
+
+    let createReceipt = await web3.eth.sendSignedTransaction(
+        createTransaction.rawTransaction
+    )
+
+    return createReceipt.transactionHash
 }
